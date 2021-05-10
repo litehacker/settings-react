2777bed58d402356232ac4727263a3743fae21f9
Giorgi Gvimradze <natceo@gmail.com>
2021-05-10 09:57:33 +0400

    discard save buttons

--- src/components/MenuContent.tsx
+++ src/components/MenuContent.tsx
@@ -3,14 +3,11 @@ import React,{FunctionComponent, useState, useEffect} from 'react';
 import { Button, Input, Text, Accordion, } from '@fluentui/react-northstar';
 import { SearchIcon, AddIcon } from '@fluentui/react-icons-northstar';
 import Data from '../data/megaMenu.json';
-type OnClick = () => void;
 
 type AccordionPanels = {
   title:string;
   content:any;
   key:string;
-  accordionTitleId?:string;
-  onClick?:OnClick;
 }[]
 
 type accordionType = {
@@ -26,8 +23,9 @@ type SubMenuProps ={
   accordionNavigation?:accordionType[]
 }
 
+var objects:AccordionPanels = [];
 
-var objects:AccordionPanels = []
+
 
 function createDuplicate(data:AccordionPanels,objectsOutput:AccordionPanels){
   if(!data){
@@ -38,7 +36,6 @@ function createDuplicate(data:AccordionPanels,objectsOutput:AccordionPanels){
       createDuplicate(inElem.content,objTmp)
      
       objectsOutput.push({
-        accordionTitleId:inElem.key, 
         key:inElem.key,
         title:inElem.title,
         ...(Array.isArray(inElem.content) ? {
@@ -55,46 +52,64 @@ function createDuplicate(data:AccordionPanels,objectsOutput:AccordionPanels){
       });
   }
 }
-createDuplicate(Data,objects)
+
+if(localStorage.getItem('Accordion')){
+  objects = JSON.parse(localStorage.getItem('Accordion') || '[]')
+}else{
+  createDuplicate(Data,objects)
+}
 
 function indexChange(input?:any){
   console.log(input);
 }
 
 const MenuContent: FunctionComponent<SubMenuProps> =  ({title, titleDescription, subTitle, subTitleDescription, accordionNavigation }) => {
-    const [,setUpdate] = useState("")
-    const [menuItem,setMenuItem] = useState("")
-    console.log(menuItem);
+  const [menuItem,setMenuItem] = useState("")
+  const [mounted, setMounted] = useState(true);
+  // Set up a piece of state, so that we have
+  // a way to trigger a re-render.
+  const [random, setRandom] = useState(Math.random());
+  console.log(menuItem);
+  const Save  = () => {
+    localStorage.setItem('Accordion', JSON.stringify(objects));
+  }
+  const Discard = () => {
+    objects = JSON.parse(localStorage.getItem('Accordion') || '[]')
+    reRender()
+  }
+  const AddEntry = () => {
+    objects.push({
+      title:menuItem,
+      key:menuItem,
+      content:menuItem
+    })
+    reRender()
 
-    const AddEntry = () => {
-      objects.push({
-        title:menuItem,
-        key:menuItem,
-        content:menuItem
-      })
-      setUpdate(menuItem)
-      console.log("add entry");
-    }
-    return(
-    <>
-      <h2 className="fs-5 fw-bolder">{title}</h2>
-      <Text size="medium" content={titleDescription} />
-      <Text weight="bold" size="large" content={subTitle} style={{paddingTop:"15px"}} />
-      <Text size="medium" content={subTitleDescription} />
-      <div  className="row pt-4 pb-5">
-        <div className="col-2">
-            <Button icon={<AddIcon size="small" />}  content="Add Entry" iconPosition="before" primary onClick={AddEntry}/>
-        </div>
-        <div className="col-4 ">
-            <Input required fluid icon={<SearchIcon/>} placeholder="Search for a navigation entry" iconPosition="end"  onChange={e=>setMenuItem((e.target as HTMLInputElement).value)} />
-        </div>
+  }
+  // This function will change the random number,
+  // and trigger a re-render (in the console,
+  // you'll see a "render!" from LifecycleDemo)
+  const reRender = () => setRandom(Math.random());
+  return(
+  <>
+    <h2 className="fs-5 fw-bolder">{title}</h2>
+    <Text size="medium" content={titleDescription} />
+    <Text weight="bold" size="large" content={subTitle} style={{paddingTop:"15px"}} />
+    <Text size="medium" content={subTitleDescription} />
+    <div  className="row pt-4 pb-5">
+      <div className="col-2">
+          <Button icon={<AddIcon size="small" />}  content="Add Entry" iconPosition="before" primary onClick={AddEntry}/>
       </div>
-      <Accordion panels={objects} exclusive onTitleClick={(e) => indexChange(e)}/>
-      <div  className="d-flex flex-row-reverse pt-5 pb-2">
-      <Button  className="mx-2" content="Save" iconPosition="before" primary />
-      <Button  content="Discard" iconPosition="before"  />
+      <div className="col-4 ">
+          <Input required fluid icon={<SearchIcon/>} placeholder="Search for a navigation entry" iconPosition="end"  onChange={e=>setMenuItem((e.target as HTMLInputElement).value)} />
       </div>
-    </>
-    )
+    </div>
+    {mounted &&<Accordion panels={objects} exclusive onTitleClick={(e) => indexChange(e)}/>}
+    <div  className="d-flex flex-row-reverse pt-5 pb-2">
+    <Button  className="mx-2" content="Save" iconPosition="before" primary onClick={Save}/>
+    <Button  content="Discard" iconPosition="before" onClick={Discard} />
+    </div>
+  </>
+  )
 }
 export default MenuContent;
